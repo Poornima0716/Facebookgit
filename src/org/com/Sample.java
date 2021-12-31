a986faf8b9e61427ab446fbd2945c04a4ced702b
Poornima0716 <poornimanive@gmail.com>
2021-12-31 16:26:40 +0530

    Username Functionality

--- src/org/com/Sample.java
+++ src/org/com/Sample.java
@@ -2,4 +2,15 @@ package org.com;
 
 public class Sample {
 
+	public static void Username() {
+		System.out.println("PoornimaAshok");
+	}
+	
+	public static void main(String[] args) {
+		Username();
+		
+	}
+	
 }
+
+
